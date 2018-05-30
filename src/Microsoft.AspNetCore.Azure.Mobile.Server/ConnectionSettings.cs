9984687fc6564c73246923381fd74a9ca1577819
為什麼 <andy840119@gmail.com>
2018-05-30 11:32:33 +0800

    add some file

---
+++ src/Microsoft.AspNetCore.Azure.Mobile.Server/ConnectionSettings.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.AspNetCore.Azure.Mobile.Server
+{
+    /// <summary>
+    /// This class provides configuration information for connection strings.
+    /// </summary>
+    public class ConnectionSettings
+    {
+        private string _name;
+        private string _connectionString;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConnectionSettings"/> with a given <paramref name="name"/>
+        /// and <paramref name="connectionString"/>.
+        /// </summary>
+        /// <param name="name">The name of the connection string setting.</param>
+        /// <param name="connectionString">The actual connection string.</param>
+        public ConnectionSettings(string name, string connectionString)
+        {
+            this._name = name ?? throw new ArgumentNullException(nameof(name));
+            this._connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+        }
+
+        /// <summary>
+        /// Gets or sets the name of the connection string.
+        /// </summary>
+        public string Name
+        {
+            get => this._name;
+
+            set => this._name = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        /// <summary>
+        /// Gets or sets the actual connection string.
+        /// </summary>
+        public string ConnectionString
+        {
+            get => this._connectionString;
+
+            set => this._connectionString = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        /// <summary>
+        /// Gets or sets the provider to be used by this connection string, e.g. <c>System.Data.SqlClient</c>.
+        /// </summary>
+        public string Provider { get; set; }
+    }
+}
---
+++ src/Microsoft.AspNetCore.Azure.Mobile.Server/Extension/StringExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.AspNetCore.Azure.Mobile.Server.Extension
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    internal static class StringExtensions
+    {
+        /// <summary>
+        /// Formats the input string to be user visible using <see cref="CultureInfo.CurrentCulture"/>.
+        /// </summary>
+        public static string FormatForUser(this string format, params object[] args)
+        {
+            return string.Format(CultureInfo.CurrentCulture, format, args);
+        }
+
+        /// <summary>
+        /// Formats the input string to be user visible using <see cref="CultureInfo.InvariantCulture"/>.
+        /// </summary>
+        public static string FormatInvariant(this string format, params object[] args)
+        {
+            return string.Format(CultureInfo.InvariantCulture, format, args);
+        }
+
+        /// <summary>
+        /// Splits a string into segments based on a given <paramref name="separator"/>. The segments are 
+        /// trimmed and empty segments are removed.
+        /// </summary>
+        /// <param name="input">The string to split.</param>
+        /// <param name="separator">An array of Unicode characters that delimit the substrings in this instance, an empty array that contains no delimiters, or null.</param>
+        /// <returns>An <see cref="IEnumerable{T}"/> containing the resulting segments.</returns>
+        public static IEnumerable<string> SplitAndTrim(this string input, params char[] separator)
+        {
+            if (input == null)
+            {
+                return Enumerable.Empty<string>();
+            }
+
+            return input.Split(separator).Select(x => x.Trim()).Where(x => !string.IsNullOrWhiteSpace(x));
+        }
+    }
+}
